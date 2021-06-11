9ab755ccf85c32abf1f25a317b7f05b3ecc1d368
Geeoon Chung <geeoon13@gmail.com>
2021-06-10 23:50:32 -0500

    added vector for average properties.

--- FluidSimulation/src/main.cpp
+++ FluidSimulation/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <array>
 #include <SFML/Graphics.hpp>
 #include <amp.h>
@@ -14,19 +15,26 @@ int main() {
 
 	while (window.isOpen()) {
 		// logic and parallel processing
-		unsigned int* pixelArray = new unsigned int[size];
-		concurrency::array_view<unsigned int, 3> pixels(xDim, yDim, 3, pixelArray);
+		std::vector<unsigned int> pixelVector;  // vector of pixels to be stored and then drawn
+		pixelVector.resize(size);
+		concurrency::array_view<unsigned int, 3> pixels(xDim, yDim, 3, pixelVector);  // must be stored in in array_view to be used in amp restricted lambda; stored as such: (x, y, [red, blue, green])
 
+		std::vector<double> averageVector;  // vector of average parts of screen, it is the same as the pixels, but it is used for storing average data as a double
+		averageVector.resize(size);
+		concurrency::array_view<double, 3> averages(xDim, yDim, 3, averageVector);  // stored as such: (x, y, [x velocity, y velocity, pressure])
+		
+		// http://graphics.cs.cmu.edu/nsp/course/15-464/Spring11/papers/StamFluidforGames.pdf
 		concurrency::parallel_for_each(pixels.extent,
-			[=](concurrency::index<3> idx) restrict(amp) {
+			[=](concurrency::index<3> idx) restrict(amp) {  // "shader"
 			int x = idx[0];
 			int y = idx[1];
-			int channel = idx[2];
-			pixels(x, y, channel) = (x + y) * 255 / (xDim + yDim);
+			int channel = idx[2];  // RGB
+			pixels(x, y, channel) = 255;
 		});
-
+		// synchronize array_view and vector
 		pixels.synchronize();
-		
+		averages.synchronize();
+
 		// checking window events
 		sf::Event e;
 		while (window.pollEvent(e)) {
@@ -45,8 +53,6 @@ int main() {
 				image.setPixel(x, y, sf::Color(pixels(x, y, 0), pixels(x, y, 1), pixels(x, y, 2)));
 			}
 		}
-
-		delete[] pixelArray;  // done with array
 
 		// loading image into texture to be drawn
 		sf::Texture t;
